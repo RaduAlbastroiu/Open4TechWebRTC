73dcaada77e04e512219c07616f3cc2279d51678
Radu Albastroiu <albastroiuradug@gmail.com>
2021-07-05 22:57:40 +0300

    Demo files

---
+++ DEMO_STAGE_1/backend/index.js
@@ -0,0 +1,16 @@
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+  cors: { origin: '*' },
+});
+
+io.on('connection', (socket) => {
+  console.log(`new user connected ${socket.id.substr(0, 5)}`);
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', `${socket.id.substr(0, 5)} said ${message}`);
+  });
+});
+
+http.listen(8080, () => console.log('listening on port 8080'));
